609233babfd9cd9d5e363c3a9f07f20522f75a18
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2024-04-20 15:05:07 +0400

    Register category with store

--- client/src/Components/StoreForm/StoreForm.js
+++ client/src/Components/StoreForm/StoreForm.js
@@ -1,10 +1,12 @@
-import { useForm } from "react-hook-form";
+import { useForm, useController } from "react-hook-form";
+import Select from "react-select";
 import "./StoreForm.css";
 
 export default function StoreForm() {
     const {
         register,
         handleSubmit,
+        control,
         formState: { errors }
     } = useForm();
 
@@ -13,9 +15,10 @@ export default function StoreForm() {
     }
 
     const onSubmit = (data) => {
+        console.log("🚀 ~ data:", data);
         const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
         };
 
@@ -27,25 +30,44 @@ export default function StoreForm() {
         });
     };
 
+    const categoryList = [
+        { value: 1, label: "womens trainers" },
+        { value: 2, label: "mens trainers" },
+        { value: 3, label: "womens dresses" },
+        { value: 4, label: "mens jackets" }
+    ];
+    const { field: { value: catValue, onChange: catOnChange, ...restLangField } } = useController({ name: "category_id", control });
+
     return(
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-control">
                     <label>Store Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        {...register("name", {
-                            required: true,
-                        })}
-                    />
+                    <div>
+                        <input
+                            type="text"
+                            name="name"
+                            style={{width: "340px", height: "24px"}}
+                            {...register("name", {
+                                required: true,
+                            })}
+                        />
+                    </div>
                     {errors.name && errors.name.type === "required" && (
                         <p className="errorMsg" style={{color: "#ec3440"}}>Name is required.</p>
                     )}
                 </div>
                 <div className="form-control">
-                    <label>Category</label>
-                    <input type="text" name="category" style={{width: "140px"}} {...register("category")} />
+                    <label>Language</label>
+                    <Select
+                        className="select-input"
+                        placeholder="Select Language"
+                        isClearable
+                        options={categoryList}
+                        value={catValue ? categoryList.find(x => x.value === catValue) : catValue}
+                        onChange={option => catOnChange(option ? option.value : option)}
+                        {...restLangField}
+                    />
                 </div>
                 <div className="form-control">
                     <label></label>
